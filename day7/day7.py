bcb3e7b1eaf05f675f90d38f401e1f12e2f62442
Nathan Flaherty <nathan@nathanflaherty.xyz>
2025-12-16 14:05:09 +0000

    completed day7 	modified:   day7.py 	modified:   ../day9/day9.py

--- day7/day7.py
+++ day7/day7.py
@@ -1,18 +1,36 @@
-f=open("test.txt")
 space=[]
 beam_indices=[]
-for line in f.readlines():
+f=open("input.txt","r")
+day7file=f.readlines()
+DP={}
+
+for i in range(len(day7file[0])):
+    for j in range(len(day7file)):
+        DP[(i,j)]=0
+max_row = j
+
+for line in day7file:
     space.append(line[:-1])
     if "S" in line:
-        beam_indices.append(line.index("S"))
+        print("1")
+        x_coord = line.index("S")
+        beam_indices.append(x_coord)
+        DP[(x_coord,0)] = 1
+print(DP)
+        
 
+
+#DP[beam] = num of timelines ending at certain beam (x,y)  
+#DP[(S,0)] = 1
+#DP [split] = DP [the one above it] + DP{the other one above it] 
 
 timelines=1
+
 print(space)
 print(beam_indices)
 beam_coords=[]
 split_count=0
-for i in range(len(space)):
+for i in range(1,len(space)):
     split=[]
     for beam in beam_indices:
         beam_coords.append((beam,i))
@@ -22,23 +40,28 @@ for i in range(len(space)):
             print("beam",beam)
             print(split)
             
+        else:
+            DP[(beam,i)] = DP[(beam,i-1)]
 
     for beam in split:
          beam_indices.remove(beam)
          beam_indices.append(beam+1)
          beam_indices.append(beam-1)
-#         withdups= len(beam_indices)
+         withdups= len(beam_indices)
          beam_indices=list(set(beam_indices))
- #        withoutdups= len(beam_indices)
-  #       if withdups> withoutdups:#there has been a merge
+         withoutdups= len(beam_indices)
          beam_coords.append((beam+1,i))
          beam_coords.append((beam-1,i))
          beam_coords.remove((beam,i))
+         DP[(beam-1,i)] += DP[(beam,i-1)] 
+         DP[(beam+1,i)] += DP[(beam,i-1)] 
+
 
 
 
 for i in range(len(space)):
     print("HERE")
+    num_beams=0
     for j in range(len(space[i])):
         if (j,i) in beam_coords:
             space[i]= space[i][:j] + "|" +space[i][j+1:]
@@ -48,7 +71,7 @@ for i in range(len(space)):
 #'for i in range(len(space)):
 
 
-#DAY 1 CODE
+#part 1 CODE
 #print(space)
 #print(beam_indices)
 #split_count=0
@@ -69,4 +92,12 @@ for i in range(len(space)):
 #        beam_indices=list(set(beam_indices))
 #print(split_count)
 
+output = 0
+for i in range(len(day7file[max_row])):
+    output += DP[(i,max_row)]
+print(output)
+    
+
+
                         
+
--- day9/day9.py
+++ day9/day9.py
@@ -17,24 +17,40 @@ for i in range(-1,len(corner_list)-1):
 
 
 x_coords = [red[0] for red in corner_list]
+x_coords.extend([green[0] for green in green_tiles])
 y_coords = [red[1] for red in corner_list]
-for j in range(max(y_coords)+1):
-    green_found = False
-    for i in range(max(x_coords)+1):
-        if not green_found and ( (i,j) in green_tiles or [i,j] in corner_list) :
-            green_found = True
-        elif green_found and ((i,j) not in green_tiles ) and [i,j] not in corner_list:
-            green_tiles.append((i,j))
-        elif green_found and ((i,j) in corner_list or (i,j) in green_tiles):
-            green_found = False # reached an extremity 
+y_coords.extend([green[1] for green in green_tiles])
+#for j in range(max(y_coords)+1):
+#    green_found = False
+#    for i in range(max(x_coords)+1):
+##        print("(",i,",",j,")"," out of ", max(x_coords),",",max(y_coords))
+#        if not green_found and ( (i,j) in green_tiles or [i,j] in corner_list) :
+#            green_found = True
+#        elif green_found and ((i,j) not in green_tiles ) and [i,j] not in corner_list:
+#            green_tiles.append((i,j))
+#        elif green_found and ((i,j) in corner_list or (i,j) in green_tiles):
+#            green_found = False # reached an extremity 
         
 def find_rec_area(coords1,coords2):
     return (abs(coords1[0] - coords2[0]) +1) * (abs(coords1[1] - coords2[1]) +1)
 def all_green(coords1,coords2):
+    #check 
     for i in range(min(coords1[0],coords2[0]),max(coords1[0],coords2[0])):
         for j in range(min(coords1[1],coords2[1]),max(coords1[1],coords2[1])):
-            if not( (i,j) in green_tiles or [i,j] in corner_list):
+            toggle = False
+            if (i,j) in green_tiles or [i,j] in corner_list:
+                Toggle = True
+                continue
+            elif any([(x_coords[k]< i and y_coords[k] == j)  for k in range(len(x_coords))]):
+                green_tiles.append((i,j) )
+                toggle = True
+                x_coords.append(i)
+                y_coords.append(j)
+            elif not toggle:
                 return False
+                # (i,j is green)
+   #         if not( (i,j) in green_tiles or [i,j] in corner_list):
+   #             return False
     return True
 
 #for j in range(max(y_coords)+2):
@@ -52,10 +68,11 @@ largest_area = 0
 for coords1 in corner_list:
     for coords2 in corner_list:
         print(coords1,coords2)
-        if coords1 != coords2 and all_green(coords1,coords2):
+        if coords1 != coords2 :
             area=find_rec_area(coords1,coords2)
             if area > largest_area:
-                largest_area = area
+                if all_green(coords1,coords2):
+                    largest_area = area
            
 
 
